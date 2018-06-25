4c42a0b17c287ab4ecc811686e2e334b449dd0a9
Pranav Rajiv <pranavrajiv@gmail.com>
2018-06-25 14:45:41 -0500

    implemented table view in the UI

--- FlagAppV2/SecondViewController.swift
+++ FlagAppV2/SecondViewController.swift
@@ -1,18 +1,30 @@
 //
-//  SecondViewController.swift
+//  secondViewController.swift
 //  FlagAppV2
 //
-//  Created by Pranav on 6/24/18.
+//  Created by Pranav on 6/25/18.
 //  Copyright © 2018 Pranav. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class secondViewController: UITableViewController {
 
+    let flags = ["india","usa","india","usa","india","usa","india","usa","india","usa","india","usa","india","usa","india","usa","india","usa"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //loadSampleFlags()
+        
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,74 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return (flags.count)
+    }
+  
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as!  flagItemTableViewCell
+
+       cell.myLabel.text =  flags[indexPath.row]
+        
+        // Configure the cell...
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
---
+++ FlagAppV2/flagItemTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  flagItemTableViewCell.swift
+//  FlagAppV2
+//
+//  Created by Pranav on 6/25/18.
+//  Copyright © 2018 Pranav. All rights reserved.
+//
+
+import UIKit
+
+class flagItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
