0b41b968a807f776af7f27f9c5be27440c2fa5bf
Pranav Rajiv <pranavrajiv@gmail.com>
2018-06-25 00:28:37 -0500

    Made a little bit of UI for the tabbed version of the app

---
+++ FlagAppV2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FlagAppV2
+//
+//  Created by Pranav on 6/24/18.
+//  Copyright © 2018 Pranav. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
